accab56f2e3f529215a64baa3f99c63cf2a816c2
Eclisher <hei.harizo@gmail.com>
2024-06-08 02:04:58 +0300

    chore: update database

--- src/main/resources/db/insert/CarI.sql
+++ src/main/resources/db/insert/CarI.sql
@@ -1,5 +1,16 @@
-INSERT INTO Car (name, description, brand, model, price, color, motor_type, power, place_number, status, type)
-VALUES
-    ('Bugatti Bolide', 'bolides le plus rapides en 2024', 'Bugatti', 'Model A', 4000000, 'Red', 'Gasoline', 1850, 4, true, 'SUV'),
-    ('Dodge Challenger', 'voiture de classe', 'Dodge', 'V8', 300000, 'Blue', 'Diesel',500, 4, false, 'Sedan'),
-    ('Bugatti Chiron', ' La 2nd à battre le record de vitesse', 'Ferrari', 'V8 bi-turbo', 2000000, 'Green', 'Electric', 1817, 5, true, 'Hatchback');
+-- Ensuite, nous insérons les données dans la table
+INSERT INTO cars (id, name, description, brand, model, price, color, motor_type, power, place_number, status, type) VALUES
+                       (1, 'Tesla Model S', 'Luxury electric sedan', 'Tesla', 'Model S', 79999.99, 'Red', 'Electric', 1020, 5, true, 'Sedan'),
+                        (2, 'Ford Mustang', 'Iconic American muscle car', 'Ford', 'Mustang GT', 55995, 'Blue', 'Gasoline', 450, 4, true, 'Coupe'),
+                       (3, 'BMW 3 Series', 'Compact executive car', 'BMW', '330i', 41300, 'Black', 'Gasoline', 255, 5, true, 'Sedan'),
+                       (4, 'Audi Q5', 'Luxury compact SUV', 'Audi', 'Q5 45 TFSI', 51900, 'White', 'Gasoline', 261, 5, true, 'SUV'),
+                       (5, 'Chevrolet Camaro', 'High-performance sports car', 'Chevrolet', 'Camaro SS', 42995, 'Yellow', 'Gasoline', 455, 4, true, 'Coupe'),
+                       (6, 'Toyota Corolla', 'Reliable and fuel-efficient compact car', 'Toyota', 'Corolla LE', 20325, 'Silver', 'Gasoline', 139, 5, true, 'Sedan'),
+                       (7, 'Honda Civic', 'Compact car with sporty handling', 'Honda', 'Civic EX', 24350, 'Gray', 'Gasoline', 180, 5, true, 'Sedan'),
+                       (8, 'Mercedes-Benz C-Class', 'Luxury compact car', 'Mercedes-Benz', 'C 300', 44100, 'Black', 'Gasoline', 255, 5, true, 'Sedan'),
+                       (9, 'Jeep Wrangler', 'Rugged off-road SUV', 'Jeep', 'Wrangler Rubicon', 44195, 'Green', 'Gasoline', 285, 5, true, 'SUV'),
+                       (10, 'Porsche 911', 'High-performance luxury sports car', 'Porsche', '911 Carrera', 101200, 'White', 'Gasoline', 379, 4, true, 'Coupe'),
+                       (11, 'Volkswagen Golf', 'Compact hatchback with versatile features', 'Volkswagen', 'Golf TSI', 23700, 'Blue', 'Gasoline', 147, 5, true, 'Hatchback'),
+                       (12, 'Lamborghini Aventador', 'Exotic high-performance sports car', 'Lamborghini', 'Aventador SVJ', 517770, 'Yellow', 'Gasoline', 759, 2, true, 'Coupe'),
+                       (13, 'Ferrari F8 Tributo', 'Supercar with exceptional performance', 'Ferrari', 'F8 Tributo', 276550, 'Red', 'Gasoline', 710, 2, true, 'Coupe'),
+                        (14,'Nissan Leaf', 'Affordable electric car','Nissan','Leaf SV', 31400 ,'White', 'Electric',147,5, true, 'Hatchback');
--- src/main/resources/db/insert/ImageI.sql
+++ src/main/resources/db/insert/ImageI.sql
@@ -1,7 +1,10 @@
-INSERT INTO Image (car_id, url)
-VALUES
-    (1, 'https://upload.wikimedia.org/wikipedia/commons/d/d0/Bugatti_Bolide_Milano.jpg'),
-    (1, 'https://www.bugatti.com/media/l5mpxsc1/iv-10-bolide-desktop.jpeg'),
-    (2, 'https://wallpapercave.com/uwp/uwp4359652.jpeg'),
-    (3, 'https://wallpapercave.com/wp/wp13385267.jpg');
-
+INSERT INTO Image (car_id, url) VALUES
+                                    (1, 'https://www.tesla.com/ownersmanual/images/GUID-5543BA62-932F-46C5-B1EF-44707D4726B2-online-en-US.png'),
+                                    (2, 'https://www.motortrend.com/uploads/sites/10/2016/06/2017-ford-mustang-v6-coupe-angular-front.png'),
+                                    (3, 'https://image.elite-auto.fr/visuel/BMW/bmw_22318imsport4wdsd7re_angularfront.png'),
+                                    (4, 'https://image.elite-auto.fr/visuel/AUDI/audi_23q5sline4wdsu2fe_angularfront.png'),
+                                    (5, 'https://inv.assets.ansira.net/ChromeColorMatch/us/TRANSPARENT_cc_2024CHC020055_01_1280_GAZ.png'),
+                                    (6, 'https://images.hgmsites.net/med/2024-toyota-corolla-se-cvt-natl-angular-front-exterior-view_100912740_m.webp'),
+                                    (7, 'https://www.topgear.com/sites/default/files/news-listicle/image/2022/08/Civic%207%20Type%20R.jpg'),
+                                    (8, 'https://www.motortrend.com/uploads/sites/10/2019/10/2020-mercedes-benz-c-class-c300-sedan-angular-front.png'),
+                                    (9, 'https://file.kelleybluebookimages.com/kbb/base/evox/CP/50481/2023-Jeep-Wrangler-front_50481_032_1917x1012_PRC_cropped.png');
